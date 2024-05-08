cd5ca095b75ba61b07877c5ee11ba4c850d14920
ilanaliouchouche <ilan.aliouchouche@universite-paris-saclay.fr>
2024-05-08 16:18:19 +0200

    endpoints fixed

--- endpoints/predict_gpu.py
+++ endpoints/predict_gpu.py
@@ -1,14 +1,24 @@
+# Imports for environment variables
+from io import BytesIO
+import os
+from dotenv import load_dotenv
+
+# Imports for web API
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-import os
-from dotenv import load_dotenv
+
+# Imports for data handling and processing
 import numpy as np
 from PIL import Image
+
+# Imports for type annotations
 from typing import List
-from io import BytesIO
+
+# Imports for deep learning and model processing
 import torch
 from transformers import pipeline, AutoModelForImageClassification, AutoImageProcessor
+
 
 # Load environment variables
 ENV_FILE_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env")
@@ -38,50 +48,72 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 pipe = pipeline('image-classification', model=model, image_processor=image_processor, device=device)
 
 # Define the endpoints
-@app.get("/")
-def index():
+@app.get("/") 
+async def index():
     return {"message": "Hello, please go to /docs to see the API documentation"}
 
 @app.get("/device")
-def get_device():
+async def get_device():
     return {"device": device}
 
 @app.get("/model")
-def get_model():
+async def get_model():
     return {"model": model_ckpt}
 
 @app.post("/predict_with_path")
-def predict_with_path(image: str):
+async def predict_with_path(image: str):
+    """
+    This function will take an image as input and return the prediction
+
+    @params {image: str} the path to the image
+    """
+
     prediction = pipe(image)
+    print(prediction)
     label = prediction[0]['label']
     score = prediction[0]['score']
+    
     return {"max_prob": score, "pred_label": label}
 
 @app.post("/predict_with_array")
-def predict_with_array(image: List[List[List[int]]]):
+async def predict_with_array(image: List[List[List[int]]]):
+    """
+    This function will take an image as input and return the prediction
+
+    @params {image: List[List[List[int]]} the image as a list of lists of lists
+    """
+
     image = np.array(image)
+
     if image.max() <= 1:
         image = image * 255
+
     image = Image.fromarray(image.astype('uint8'))
+
     prediction = pipe(image)
+
+    print(prediction)
     label = prediction[0]['label']
     score = prediction[0]['score']
+
     return {"max_prob": score, "pred_label": label}
 
 @app.post("/predict_with_file")
 async def predict_with_file(file: UploadFile = File(...)):
-    # Read the image file and convert it into a PIL Image
+    """
+    This function will take an image as input and return the prediction
+
+    @params {file: UploadFile} the image file
+    """
+
     image_contents = await file.read()
     image = Image.open(BytesIO(image_contents))
     
-    # Convert the PIL Image into a numpy array
     image_array = np.array(image)
     
     if image_array.shape[2] > 1:
-        # Convert to grayscale by averaging channels
         image_array = np.mean(image_array, axis=2)
     
-    # Scale the values if they are between 0 and 1
     if image_array.max() <= 1:
         image_array *= 255
 
@@ -90,5 +122,7 @@ async def predict_with_file(file: UploadFile = File(...)):
     score = prediction[0]['score']
     return {"max_prob": score, "pred_label": label}
 
+
 if __name__ == "__main__":
-    uvicorn.run(app, host=host, port=port)
+    uvicorn.run(app, host=host,
+                port=port)
--- endpoints/predict_mps.py
+++ endpoints/predict_mps.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 
 # Imports for web API
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 # Imports for data handling and processing
@@ -26,10 +27,20 @@ load_dotenv(dotenv_path=ENV_FILE_PATH)
 model_ckpt = os.getenv("MODEL_CKPT")
 host = os.getenv("HOST")
 port = int(os.getenv("PORT_ENDPOINT"))
+app_port = int(os.getenv("PORT_APP"))
 
 
 # Initialize the FastAPI app
 app = FastAPI()
+
+# Set up CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[f"http://{host}:{app_port}"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST"], 
+    allow_headers=["*"], 
+)
 
 model = AutoModelForImageClassification.from_pretrained(model_ckpt)
 image_processor = AutoImageProcessor.from_pretrained(model_ckpt)
@@ -40,19 +51,19 @@ pipe = pipeline('image-classification', model=model, image_processor=image_proce
 
 # Define the endpoints
 @app.get("/") 
-def index():
+async def index():
     return {"message": "Hello, please go to /docs to see the API documentation"}
 
 @app.get("/device")
-def get_device():
+async def get_device():
     return {"device": device}
 
 @app.get("/model")
-def get_model():
+async def get_model():
     return {"model": model_ckpt}
 
 @app.post("/predict_with_path")
-def predict_with_path(image: str):
+async def predict_with_path(image: str):
     """
     This function will take an image as input and return the prediction
 
@@ -67,7 +78,7 @@ def predict_with_path(image: str):
     return {"max_prob": score, "pred_label": label}
 
 @app.post("/predict_with_array")
-def predict_with_array(image: List[List[List[int]]]):
+async def predict_with_array(image: List[List[List[int]]]):
     """
     This function will take an image as input and return the prediction
 
@@ -91,18 +102,20 @@ def predict_with_array(image: List[List[List[int]]]):
 
 @app.post("/predict_with_file")
 async def predict_with_file(file: UploadFile = File(...)):
-    # Read the image file and convert it into a PIL Image
+    """
+    This function will take an image as input and return the prediction
+
+    @params {file: UploadFile} the image file
+    """
+
     image_contents = await file.read()
     image = Image.open(BytesIO(image_contents))
     
-    # Convert the PIL Image into a numpy array
     image_array = np.array(image)
     
     if image_array.shape[2] > 1:
-        # Convert to grayscale by averaging channels
         image_array = np.mean(image_array, axis=2)
     
-    # Scale the values if they are between 0 and 1
     if image_array.max() <= 1:
         image_array *= 255
 
