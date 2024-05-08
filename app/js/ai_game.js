202473cd36a6c6db75c4908447a9acf9a08cd632
Ilyes Djerfaf <87201310+idjerfaf@users.noreply.github.com>
2024-05-08 20:14:45 +0200

    Quick-Off : AI Game Dev

---
+++ app/js/ai_game.js
@@ -0,0 +1,162 @@
+const canvas = document.getElementById('drawCanvas');
+const ctx = canvas.getContext('2d', { willReadFrequently: true });
+canvas.width = 400;
+canvas.height = 400;
+let painting = false;
+let intervalId = null;
+let timerInterval = null; // Global timer interval
+let currentRound = 1;
+let totalRounds = parseInt(new URLSearchParams(window.location.search).get('totalRounds'));
+let scores = new Array(totalRounds).fill(0); // Initialize scores array
+let X = null;
+const speed = new URLSearchParams(window.location.search).get('difficulty') === 'hard' ? 100 : 30;
+let mean_time_player = 0;
+let score_player = 0;
+let start_time = 0;
+const player_name = new URLSearchParams(window.location.search).get('playerName');
+const difficulty = new URLSearchParams(window.location.search).get('difficulty');
+
+
+const prompts = {
+    "airplane": "✈️", "banana": "🍌", "computer": "💻", "dog": "🐶", "elephant": "🐘",
+    "fish": "🐟", "garden": "🌼", "helmet": "⛑️", "ice cream": "🍦", "jail": "🏛️",
+    "key": "🔑", "lantern": "🏮", "motorbike": "🏍️", "necklace": "📿", "onion": "🧅",
+    "penguin": "🐧", "raccoon": "🦝", "sandwich": "🥪", "table": "🪑", "underwear": "🩲",
+    "vase": "🏺", "watermelon": "🍉", "yoga": "🧘", "zigzag": "〰️"
+};
+
+function selectRandomPrompt() {
+    const promptKeys = Object.keys(prompts);
+    const randomKey = promptKeys[Math.floor(Math.random() * promptKeys.length)];
+    const randomPrompt = `${prompts[randomKey]} ${randomKey.charAt(0).toUpperCase() + randomKey.slice(1)}`;
+    X = prompts[randomKey];
+    document.getElementById('randomPrompt').innerText = randomPrompt;
+}
+
+function initializeTimer() {
+    // in this function i want the timer to start at 0 and count up
+    clearInterval(timerInterval); // Clear existing timer
+    let timer = 0, minutes, seconds;
+    start_time = new Date().getTime();
+    const countdown = document.getElementById('countdown');
+    timerInterval = setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        countdown.textContent = minutes + ":" + seconds;
+        timer++;
+    }, 1000);
+}
+
+function finishRound() {
+    const end_time = new Date().getTime();
+    const time_diff = end_time - start_time;
+    mean_time_player = mean_time_player + time_diff;
+    callPredictionAPI();
+    const scoreForRound = document.getElementById('predictionText').innerText.includes(X) ? 1 : 0;
+    updateScore(scoreForRound);
+}
+
+function updateScore(newScore) {
+    scores[currentRound - 1] = newScore;
+    document.getElementById('currentRound').innerText = `Round ${currentRound}: Your score is ${newScore}`;
+    document.getElementById('previousScores').innerText = `Total Score: ${scores.reduce((a, b) => a + b, 0)}`;
+    if (currentRound < totalRounds) {
+        currentRound++;
+        resetGameForNextRound();
+    } else {
+        finishGame();
+    }
+}
+
+function finishGame() {
+    mean_time_player = mean_time_player / totalRounds;
+    mean_time_player = mean_time_player / 1000;
+    window.location.href = `end_clock_game.html?score=${scores.reduce((a, b) => a + b, 0)}&mean_time=${mean_time_player}&player_name=${player_name}&difficulty=${difficulty}&totalRounds=${totalRounds}`;
+}
+
+function resetGameForNextRound() {
+    selectRandomPrompt();
+    initializeCanvas();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    initializeTimer();
+}
+
+function startPosition(e) {
+    painting = true;
+    draw(e);
+    if (!intervalId) intervalId = setInterval(callPredictionAPI, 1500);
+}
+
+function finishedPosition() {
+    painting = false;
+    clearInterval(intervalId);
+    intervalId = null;
+    ctx.beginPath();
+    callPredictionAPI();
+}
+
+function draw(e) {
+    if (!painting) return;
+    ctx.lineWidth = 5;
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = 'black';
+    const rect = canvas.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+}
+
+
+
+function initializeCanvas() {
+    canvas.addEventListener('mousedown', startPosition);
+    canvas.addEventListener('mouseup', finishedPosition);
+    canvas.addEventListener('mousemove', draw);
+}
+
+function dataURItoBlob(dataURI) {
+    const byteString = atob(dataURI.split(',')[1]);
+    const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+    const ia = new Uint8Array(byteString.length);
+    for (let i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+    return new Blob([ia], { type: mimeString });
+}
+
+function extractImage() {
+    const image = canvas.toDataURL('image/png');
+    return dataURItoBlob(image);
+}
+
+function callPredictionAPI() {
+    const imageBlob = extractImage();
+    const formData = new FormData();
+    formData.append('file', imageBlob);
+    fetch('http://127.0.0.1:8000/predict_with_file', {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => response.json())
+        .then(data => {
+            const emoji = prompts[data.pred_label];
+            const predictionText = `Prediction: ${emoji}`;
+            document.getElementById('predictionText').innerText = predictionText;
+            if (predictionText.includes(X)) {
+                finishRound();
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            document.getElementById('predictionText').innerText = "Error making prediction";
+        });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    resetGameForNextRound();
+});
--- app/js/game.js
+++ app/js/game.js
@@ -3,5 +3,11 @@ document.getElementById('gameSetup').addEventListener('submit', function (event)
     const playerName = document.getElementById('playerName').value;
     const totalRounds = parseInt(document.getElementById('rounds').value);
     const difficulty = document.getElementById('difficulty').value;
-    window.location.href = `clock_game.html?playerName=${encodeURIComponent(playerName)}&totalRounds=${totalRounds}&difficulty=${difficulty}`;
+    const params = new URLSearchParams(window.location.search);
+    const mode = params.get('mode');
+    if (mode === 'time') {
+        window.location.href = `clock_game.html?playerName=${encodeURIComponent(playerName)}&totalRounds=${totalRounds}&difficulty=${difficulty}`;
+    } else if (mode === 'ai') {
+        window.location.href = `ai_game.html?playerName=${encodeURIComponent(playerName)}&totalRounds=${totalRounds}&difficulty=${difficulty}`;
+    }
 });
