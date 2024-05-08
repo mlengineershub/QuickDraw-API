d49c4bca420d214d20442108df48a7ca814b243f
Ilyes Djerfaf <87201310+idjerfaf@users.noreply.github.com>
2024-05-08 13:53:55 +0200

    Improve front end v0.0

--- app/js/game.js
+++ app/js/game.js
@@ -1,14 +1,3 @@
-// document.getElementById('gameSetup').addEventListener('submit', function (event) {
-//     event.preventDefault();
-
-//     const playerName = document.getElementById('playerName').value; 
-//     const difficulty = document.getElementById('difficulty').value;
-//     const rounds = document.getElementById('rounds').value; 
-
-//     window.location.href = 'clock_game.html?difficulty=' + difficulty;
-// });
-
-
 document.getElementById('gameSetup').addEventListener('submit', function (event) {
     event.preventDefault();
     const playerName = document.getElementById('playerName').value;
