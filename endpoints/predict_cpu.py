b83e3ffe7f4d493442fab383e4649c32b87dc6fe
Ilyes Djerfaf <87201310+idjerfaf@users.noreply.github.com>
2024-05-11 22:30:07 +0200

    Flake8

--- endpoints/predict_cpu.py
+++ endpoints/predict_cpu.py
@@ -16,11 +16,15 @@ from PIL import Image
 from typing import List
 
 # Imports for deep learning and model processing
-from transformers import pipeline, AutoModelForImageClassification, AutoImageProcessor
+from transformers import (
+    pipeline, AutoModelForImageClassification, AutoImageProcessor
+)
 
 
 # Load environment variables
-ENV_FILE_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env")
+ENV_FILE_PATH = (
+    os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env")
+)
 load_dotenv(dotenv_path=ENV_FILE_PATH)
 
 model_ckpt = os.getenv("MODEL_CKPT")
@@ -37,29 +41,36 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[f"http://{host}:{app_port}"],
     allow_credentials=True,
-    allow_methods=["GET", "POST"], 
-    allow_headers=["*"], 
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"]
 )
 
 model = AutoModelForImageClassification.from_pretrained(model_ckpt)
 image_processor = AutoImageProcessor.from_pretrained(model_ckpt)
 
 device = "cpu"
-pipe = pipeline('image-classification', model=model, image_processor=image_processor, device=device)
+pipe = pipeline('image-classification',
+                model=model,
+                image_processor=image_processor,
+                device=device)
 
 
 # Define the endpoints
-@app.get("/") 
+@app.get("/")
 async def index():
-    return {"message": "Hello, please go to /docs to see the API documentation"}
+    return {"message":
+            "Hello, please go to /docs to see the API documentation"}
+
 
 @app.get("/device")
 async def get_device():
     return {"device": device}
 
+
 @app.get("/model")
 async def get_model():
     return {"model": model_ckpt}
+
 
 @app.post("/predict_with_path")
 async def predict_with_path(image: str):
@@ -73,8 +84,9 @@ async def predict_with_path(image: str):
     print(prediction)
     label = prediction[0]['label']
     score = prediction[0]['score']
-    
+
     return {"max_prob": score, "pred_label": label}
+
 
 @app.post("/predict_with_array")
 async def predict_with_array(image: List[List[List[int]]]):
@@ -99,6 +111,7 @@ async def predict_with_array(image: List[List[List[int]]]):
 
     return {"max_prob": score, "pred_label": label}
 
+
 @app.post("/predict_with_file")
 async def predict_with_file(file: UploadFile = File(...)):
     """
@@ -109,12 +122,12 @@ async def predict_with_file(file: UploadFile = File(...)):
 
     image_contents = await file.read()
     image = Image.open(BytesIO(image_contents))
-    
+
     image_array = np.array(image)
-    
+
     if image_array.shape[2] > 1:
         image_array = np.mean(image_array, axis=2)
-    
+
     if image_array.max() <= 1:
         image_array *= 255
 
