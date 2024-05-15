3354128de74898d24a30ffda0876dd9d3ae2c5b6
FirePower-Git <58250403+FirePower-Git@users.noreply.github.com>
2024-05-15 21:53:34 +0200

    Update game.js
    Add comments

--- app/js/game.js
+++ app/js/game.js
@@ -1,13 +1,24 @@
-document.getElementById('gameSetup').addEventListener('submit', function (event) {
+
+/**
+ * Submit the game settings to the server with a custom url redirection
+ */
+document.getElementById('gameSetup').addEventListener('submit', (event) => {
     event.preventDefault();
+
+    // Get game informations
     const playerName = document.getElementById('playerName').value;
     const totalRounds = parseInt(document.getElementById('rounds').value);
     const difficulty = document.getElementById('difficulty').value;
     const params = new URLSearchParams(window.location.search);
     const mode = params.get('mode');
+
+    // Build url params
+    const url_params = `clock_game.html?playerName=${encodeURIComponent(playerName)}&totalRounds=${totalRounds}&difficulty=${difficulty}`;
+    
+    // Redirect
     if (mode === 'time') {
-        window.location.href = `clock_game.html?playerName=${encodeURIComponent(playerName)}&totalRounds=${totalRounds}&difficulty=${difficulty}`;
+        window.location.href = `clock_game.html?${url_params}`;
     } else if (mode === 'ai') {
-        window.location.href = `ai_game.html?playerName=${encodeURIComponent(playerName)}&totalRounds=${totalRounds}&difficulty=${difficulty}`;
+        window.location.href = `ai_game.html?${url_params}`;
     }
 });
