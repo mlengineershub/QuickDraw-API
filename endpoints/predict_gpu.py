986ab5ebe3e838bd33652068d144495132b9834b
ilanaliouchouche <ilan.aliouchouche@universite-paris-saclay.fr>
2024-05-20 14:59:50 +0200

    cors updated

--- endpoints/predict_gpu.py
+++ endpoints/predict_gpu.py
@@ -51,8 +51,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST"],
-    allow_headers=["*"]
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 model = AutoModelForImageClassification.from_pretrained(model_ckpt)
--- endpoints/predict_mps.py
+++ endpoints/predict_mps.py
@@ -52,8 +52,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST"],
-    allow_headers=["*"]
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 model = AutoModelForImageClassification.from_pretrained(model_ckpt)
